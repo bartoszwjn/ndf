3262cc89f11bc99e338dac26b7fd8bd51433005e
Bartosz Wojno <bartoszwjn@gmail.com>
2025-05-10 21:42:15 +0200

    build: init Cargo project

---
+++ package.nix
@@ -0,0 +1,19 @@
+{ lib, rustPlatform }:
+let
+  cargoToml = lib.importTOML ./Cargo.toml;
+in
+rustPlatform.buildRustPackage {
+  pname = cargoToml.package.name;
+  version = cargoToml.package.version;
+
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./Cargo.toml
+      ./Cargo.lock
+      ./src
+    ];
+  };
+
+  cargoLock.lockFile = ./Cargo.lock;
+}
