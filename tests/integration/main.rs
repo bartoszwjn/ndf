be38534e8798f4b27f03059adcefc86a3d5465cc
Bartosz Wojno <bartoszwjn@gmail.com>
2026-05-11 02:22:18 +0200

    test: add test cases for integration tests

--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -8,7 +8,6 @@ mod test_case;
 
 static EYRE_INIT: Once = Once::new();
 
-#[allow(dead_code)]
 fn run_test_case(name: &str) -> eyre::Result<()> {
     EYRE_INIT.call_once(|| color_eyre::install().unwrap());
     TestCase::run(name)
@@ -27,5 +26,29 @@ macro_rules! test_cases {
 
 test_cases!(
     // keep-sorted start
+    basic_file,
+    basic_flake,
+    different_outputs_file,
+    different_outputs_flake,
+    dirty_worktree_file,
+    dirty_worktree_flake,
+    empty_repo_file,
+    empty_repo_flake,
+    function_file,
+    manual_selection_file,
+    manual_selection_flake,
+    nixos_file,
+    nixos_flake,
+    nixos_manual_selection_file,
+    nixos_manual_selection_flake,
+    no_changes_file,
+    no_changes_flake,
+    no_outputs_file,
+    no_outputs_flake,
+    search_up_for_flake,
+    subdir_file,
+    subdir_flake,
+    weird_names_file,
+    weird_names_flake,
     // keep-sorted end
 );
